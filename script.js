08b8a226a24225e5edce41d3d7049371321d95b4
CebotariTigran <cebotaritigran@gmail.com>
2023-03-07 21:55:37 +0100

    Add remove button to each book

--- script.js
+++ script.js
@@ -16,25 +16,55 @@ function addBookToLibrary(event) {
     const pages = document.querySelector('#pages').value;
     const read = document.querySelector('#read').checked;
     const book = new Book(title, author, pages, read)
-    console.log(book)
     bookLibrary.push(book);
+    console.log(bookLibrary)
     for (let i = bookLibrary.length - 1; i < bookLibrary.length; i++) {
         const node = document.createElement('p');
         const book = document.createTextNode(bookLibrary[i]);
+        const removeButton = document.createElement('button');
+        const div = document.createElement('div');
+        removeButton.textContent = 'Remove'
+        removeButton.setAttribute('class', 'remove-button')
+        removeButton.setAttribute('value', i)
+        div.setAttribute('id', i);
+        div.setAttribute('class', i);
         node.appendChild(book);
-        books.appendChild(node);
+        div.appendChild(node);
+        div.appendChild(removeButton);
+        books.appendChild(div);
     }
     event.preventDefault();
     form.style.visibility = 'hidden';
+
+    const removeButton = document.querySelectorAll('.remove-button');
+    if (bookLibrary.length > 0) {
+        for (let k = 0; k < removeButton.length; k++) {
+            removeButton[k].addEventListener('click', () => {
+                position = parseInt(removeButton[k].value);
+                let tst = position.toString();
+                bookLibrary.splice(position, 1);
+                const toRemove = document.getElementById(tst);
+                if (toRemove) {
+                    toRemove.remove();
+                }
+                console.log(bookLibrary)
+            })
+        }
+    }
 }
 
+
+
+
 const submitBook = document.querySelector('.book-add-button');
-submitBook.addEventListener('click', addBookToLibrary)
+submitBook.addEventListener('click', addBookToLibrary);
+
+
 
 const showForm = document.querySelector('.show-form');
 const form = document.querySelector('.button-container')
 showForm.addEventListener('click', () => {
-    if (form.style.visibility == 'hidden') {
+    if (window.getComputedStyle(form).visibility == 'hidden') {
         form.style.visibility = 'visible';
     } else {
         form.style.visibility = 'hidden';

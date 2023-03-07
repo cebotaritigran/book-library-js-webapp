8138db6213750c9b39e7a368487247e663b2e52f
CebotariTigran <cebotaritigran@gmail.com>
2023-03-07 17:05:24 +0100

    Add base index format and script

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let bookLibrary = ["harry potter","witcher"];
+let bookLibrary = ["book number 1","book number 2"];
+
+const books = document.querySelector('.books-grid');
 
 function Book() {
 
 }
 
 function addBookToLibrary() {
-
+    for(let i = 0; i < bookLibrary.length; i++) {
+        const node = document.createElement('p');
+        const book = document.createTextNode(bookLibrary[i]);
+        node.appendChild(book);
+        books.appendChild(node);
+    }
 }
+
+const button = document.querySelector('.book-add-button');
+button.addEventListener('click',addBookToLibrary)

57417e3e41f8c7ee9b821c16da3df53899d67756
CebotariTigran <cebotaritigran@gmail.com>
2023-03-07 18:17:42 +0100

    Add function to be able to add books

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-let bookLibrary = ["book number 1","book number 2"];
+let bookLibrary = [];
 
 const books = document.querySelector('.books-grid');
 
-function Book() {
-
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    return [this.title, this.author, this.pages, this.read];
 }
 
-function addBookToLibrary() {
-    for(let i = 0; i < bookLibrary.length; i++) {
+function addBookToLibrary(event) {
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const read = document.querySelector('#read').value;
+    const book = new Book(title, author, pages, read)
+    console.log(book)
+    bookLibrary.push(book);
+    for (let i = bookLibrary.length - 1; i < bookLibrary.length; i++) {
         const node = document.createElement('p');
         const book = document.createTextNode(bookLibrary[i]);
         node.appendChild(book);
         books.appendChild(node);
     }
+    event.preventDefault();
 }
 
+
+
+
 const button = document.querySelector('.book-add-button');
-button.addEventListener('click',addBookToLibrary)
+button.addEventListener('click', addBookToLibrary)

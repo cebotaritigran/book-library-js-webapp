f5cb8ca702df129c67b305059c96276dbfc61627
CebotariTigran <cebotaritigran@gmail.com>
2023-03-08 11:20:47 +0100

    Update script and style

--- script.js
+++ script.js
@@ -13,18 +13,23 @@ function addBookToLibrary(event) {
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const pages = document.querySelector('#pages').value;
+    const checked = document.querySelector('#read').checked;
     const book = new Book(title, author, pages)
-
+    console.log(book[0]);
     bookLibrary.push(book);
+    const titleBook = document.createTextNode(book[0]);
+    const authorBook = document.createTextNode(book[1]);
+    const pagesBook = document.createTextNode(book[2]);
 
     for (let i = bookLibrary.length - 1; i < bookLibrary.length; i++) {
-        const node = document.createElement('p');
-        const book = document.createTextNode(bookLibrary[i]);
+        const nodeTitle = document.createElement('p');
+        const nodeAuthor = document.createElement('p');
+        const nodePages = document.createElement('p');
         const removeButton = document.createElement('button');
         const readButton = document.createElement('button');
         const div = document.createElement('div');
 
-        readButton.textContent = 'Not Read';
+
         readButton.setAttribute('class', 'read-button')
         readButton.setAttribute('value', 'false');
         removeButton.textContent = 'Remove';
@@ -34,15 +39,43 @@ function addBookToLibrary(event) {
         div.setAttribute('id', i);
         div.setAttribute('class', 'card');
 
+        if (checked) {
+            readButton.classList.remove('have-read');
+            readButton.textContent = 'Read';
+        } else {
+            readButton.classList.add('have-read');
+            readButton.textContent = 'Not Read';
+        }
 
-        // apparently this works to look into tommorow
-        // i could have finished this much sooner
-        readButton.addEventListener('click',() => {
-            readButton.classList.toggle('have-read');
+        readButton.addEventListener('click', () => {
+            if (readButton.value == 'false') {
+                readButton.classList.remove('have-read');
+                readButton.textContent = 'Read';
+                readButton.value = 'true';
+            } else {
+                readButton.classList.add('have-read');
+                readButton.textContent = 'Not Read';
+                readButton.value = 'false';
+            }
+
         })
 
-        node.appendChild(book);
-        div.appendChild(node);
+        removeButton.addEventListener('click', () => {
+            position = parseInt(removeButton.value);
+            let positionText = position.toString();
+            bookLibrary.splice(position, 1);
+            const toRemove = document.getElementById(positionText);
+            if (toRemove) {
+                toRemove.remove();
+            }
+        })
+
+        nodeTitle.appendChild(titleBook);
+        nodeAuthor.appendChild(authorBook);
+        nodePages.appendChild(pagesBook);
+        div.appendChild(nodeTitle);
+        div.appendChild(nodeAuthor)
+        div.appendChild(nodePages)
         div.appendChild(readButton)
         div.appendChild(removeButton);
         books.appendChild(div);
@@ -51,39 +84,11 @@ function addBookToLibrary(event) {
     form.style.visibility = 'hidden';
 }
 
-const readButton = document.querySelectorAll('.read-button');
-for (let i = 0; i < readButton.length; i++) {
-    readButton[i].addEventListener('click', () => {
-        readButton[i].classList.toggle('have-read');
-    }, { once: true })
-}
-
-const removeButton = document.querySelectorAll('.remove-button');
-if (bookLibrary.length > 0) {
-    for (let k = 0; k < bookLibrary.length; k++) {
-        removeButton[k].addEventListener('click', () => {
-            position = parseInt(removeButton[k].value);
-            let positionText = position.toString();
-            bookLibrary.splice(position, 1);
-            const toRemove = document.getElementById(positionText);
-            if (toRemove) {
-                toRemove.remove();
-            }
-            console.log(bookLibrary.length)
-        }, { once: true })
-    }
-}
-
-// books.addEventListener('click', function(e) {
-//     if(e.target.classList.contains('read-button')){}
-    
-// })
-
-const submitBook = document.querySelector('.book-add-button');
-submitBook.addEventListener('click', addBookToLibrary);
+const submitBook = document.querySelector('.form');
+submitBook.addEventListener('submit', addBookToLibrary);
 
 const showForm = document.querySelector('.show-form');
-const form = document.querySelector('.button-container')
+const form = document.querySelector('.new-book-form')
 showForm.addEventListener('click', () => {
     if (window.getComputedStyle(form).visibility == 'hidden') {
         form.style.visibility = 'visible';
